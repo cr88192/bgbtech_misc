fc6d0bf8fc9937e7e98d7bc776779b06f2c9a81d
cr88192 <cr88192@gmail.com>
2015-12-08 10:04:18 -0600

    added BTIC1H conv-shim stubs.

---
+++ tst_btic1h/bt1h_shim_bc7.c
@@ -0,0 +1,6 @@
+/*
+ * TBD: Fast Shim decoder to BC7.
+ * Goal will be to quickly shim MetaBlock2 blocks to analogous S2TC blocks.
+ * Conversion Speed will be the priority.
+ */
+
---
+++ tst_btic1h/bt1h_shim_s2tc.c
@@ -0,0 +1,5 @@
+/*
+ * TBD: Fast Shim decoder to S2TC.
+ * Goal will be to quickly shim MetaBlock2 blocks to analogous S2TC blocks.
+ * Conversion Speed will be the priority.
+ */
